fab3226ed490aac2e2cd459fe464cf9f86940156
Mateusz Szkudlarek <mateuszszkudlarek5@gmail.com>
2022-04-29 21:01:00 +0200

    Plik clear.sql

---
+++ clear.sql
@@ -0,0 +1,18 @@
+BEGIN;
+
+DROP TABLE IF EXISTS sektory cascade;
+DROP TABLE IF EXISTS mecze_rodzaje cascade;
+DROP TABLE IF EXISTS mecze cascade;
+DROP TABLE IF EXISTS bilety_ceny cascade;
+DROP TABLE IF EXISTS kibice cascade;
+DROP TABLE IF EXISTS bilety cascade;
+DROP TABLE IF EXISTS fancluby cascade;
+DROP TABLE IF EXISTS fancluby_kibice cascade;
+DROP TABLE IF EXISTS karnety_ceny cascade;
+DROP TABLE IF EXISTS karnety cascade;
+DROP TABLE IF EXISTS zwolnione_karnety cascade;
+DROP TABLE IF EXISTS sektory_restrykcje cascade;
+DROP TABLE IF EXISTS kibice_restrykcje cascade;
+DROP TABLE IF EXISTS sektory_rzedy cascade;
+
+COMMIT;
